b603b20ac4fafd5b3f45e2f7fc9899f744e5967d
baiguangyu <490000133@qq.com>
2019-11-08 17:22:46 +0800

    init

---
+++ arya-eureka/src/main/java/com/javabgy/aryaeureka/AryaEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.javabgy.aryaeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AryaEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AryaEurekaApplication.class, args);
+    }
+
+}
---
+++ arya-ucenter/src/main/java/com/javabgy/aryaucenter/AryaUcenterApplication.java
@@ -0,0 +1,13 @@
+package com.javabgy.aryaucenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AryaUcenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AryaUcenterApplication.class, args);
+    }
+
+}
