a592fea6c2786ef74f921cf0c95d0ade3b3fe74f
490000133@qq.com <shmdongxi.1201>
2019-11-29 16:51:45 +0800

    api init

---
+++ arya-api/src/main/java/com/javabgy/aryaapi/AryaApiApplication.java
@@ -0,0 +1,13 @@
+package com.javabgy.aryaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AryaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AryaApiApplication.class, args);
+    }
+
+}
